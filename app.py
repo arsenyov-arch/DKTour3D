b8a6d5c64683c1de14529bc34bc6c0e1d48a0515
sasha <sanyademka0@gmail.com>
2026-04-06 10:54:48 +0500

    add static, templates, change app.py

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index_page():
-    return "hello"
+    return render_template("Home.html")
+
+@app.route("/kvant")
+def kvant_page():
+    return render_template("KVANT.html")
+
+@app.route("/dzhd")
+def dzhd_page():
+    return render_template("DZHD.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0")
